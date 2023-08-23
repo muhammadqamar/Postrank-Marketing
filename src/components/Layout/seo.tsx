a605ff9ae90e8baf14592b15de6f4cf94c9cd22e
muhammadqamar <muhammadqamar111@gmail.com>
2023-08-23 19:17:25 +0500

    add author name in config

--- src/components/Layout/seo.tsx
+++ src/components/Layout/seo.tsx
@@ -28,16 +28,15 @@ function Seo({ description, title, children }) {
 
   return (
     <>
-      {/* <title>{defaultTitle ? `${title} | ${defaultTitle}` : "Postrank"}</title> */}
-      <title>{"Postrank"}</title>
-      <meta name="description" content={"Marketing website"} />
-      <meta property="og:title" content={"Postrank"} />
-      <meta property="og:description" content={"Marketing website"} />
+      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
+      <meta name="description" content={metaDescription} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
       <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={"Marketing website"} />
+      <meta name="twitter:description" content={metaDescription} />
       {children}
     </>
   )
